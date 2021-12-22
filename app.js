527cb072fbeff5ae2bff6fb2c3d07b8ae502ad36
Jean Pierre Leyton <jp.leytonreyes@gmail.com>
2021-12-22 15:45:24 -0300

    Repetir el menú de forma infinita y manual

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ console.clear();
 
 const main = async() =>{
     console.log('HOLA');
-    mostrarMenu();
-    pausa();
+    let opt = '';
+    do {        
 
+     opt =  await mostrarMenu();
+     console.log({opt});
+     if( opt !== '0') await pausa();
+
+    } while (opt !== '0');
 }
 
 main();
